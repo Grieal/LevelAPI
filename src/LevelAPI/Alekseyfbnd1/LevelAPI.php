ec16e92ecd42cc6b16555077d9d1761a9419d0ca
Grieal <75247816+Grieal@users.noreply.github.com>
2020-12-04 12:48:11 +0300

    Update LevelAPI.php

--- src/LevelAPI/Alekseyfbnd1/LevelAPI.php
+++ src/LevelAPI/Alekseyfbnd1/LevelAPI.php
@@ -1,1 +1,76 @@
+<?php
 
+namespace LevelAPI\Alekseyfbnd1;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\{Server, Player};
+
+class LevelAPI extends PluginBase {
+  
+  public $users;
+  
+  public function onLoad() :void {
+    $commands = [
+      new MyLevelCommand($this, "mylevel", "твой уровень", "mylevel.command"),
+      new MyLevelCommand($this, "mypoint", "твой опыт", "mypoint.command"),
+      new MyLevelCommand($this, "toplevel", "топ уровней", "toplevel.command"),
+      new MyLevelCommand($this, "toppoint", "топ опыта", "toppoint.command")
+    ];
+    
+    foreach ($commands as $command) {
+      $this->getServer()->getCommandMap()->register("LevelAPI", $command); 
+    }
+  }
+  
+  public function onEnable () :void {
+    $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+    if (!is_dir($this->getDataFolder())) {
+      mkdir($this->getDataFolder());
+    }
+    
+    $this->users = new SQLite3($this, "users");
+  }
+  
+  public function getLevel (string $player) :string {
+    $this->users->prepare("SELECT * FROM users WHERE name = :name"); 
+    $this->users->bind(":name", $player);
+    $this->users->execute();
+    return $this->users->get()[0]["level"];
+  }
+  
+  public function getPoint (string $player) :string {
+    $this->users->prepare("SELECT * FROM users WHERE name = :name"); 
+    $this->users->bind(":name", $player);
+    $this->users->execute();
+    return $this->users->get()[0]["point"];
+  }
+  
+  public function reduceLevel (string $player, int $count) :void {
+    $this->users->prepare("UPDATE users SET level = :level WHERE name = :name"); 
+    $this->users->bind(":level", $this->getLevel($player) - $count);
+    $this->users->bind(":name", $player);
+    $this->users->execute();
+  }
+  
+  public function reducePoint (string $player, int $count) :void {
+    $this->users->prepare("UPDATE users SET point = :point WHERE name = :name"); 
+    $this->users->bind(":point", $this->getPoint($player) - $count);
+    $this->users->bind(":name", $player);
+    $this->users->execute();
+  }
+  
+  public function addLevel (string $player, int $count) :void {
+    $this->users->prepare("UPDATE users SET level = :level WHERE name = :name"); 
+    $this->users->bind(":level", $this->getLevel($player) + $count);
+    $this->users->bind(":name", $player);
+    $this->users->execute();
+  }
+  
+  public function addPoint (string $player, int $count) :void {
+    $this->users->prepare("UPDATE users SET point = :point WHERE name = :name"); 
+    $this->users->bind(":point", $this->getPoint($player) + $count);
+    $this->users->bind(":name", $player);
+    $this->users->execute();
+  }
+  
+}
